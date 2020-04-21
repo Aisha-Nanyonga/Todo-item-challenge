04cbd89849289375cb0d0a54c441168a0ecdc962
Aisha-Nanyonga <aishany.24@gmail.com>
2020-04-21 16:46:05 -0700

    html css and javascript for todo app

--- UI/Templates/todo.js
+++ UI/Templates/todo.js
@@ -18,26 +18,3 @@ list.addEventListener('click', function(ev) {
 
 
 // Click add to add item
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("itemBox").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-  alert("Add item first...");
-  } else {
-    document.getElementById("list-item").appendChild(li);
-  }
-  document.getElementById("itemBox").value = "";
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-    }
-  }
-}
